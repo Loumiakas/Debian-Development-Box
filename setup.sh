fdcc31b1101e0b24b86d7b948a76340beaa02f7a
Ruslanas Laskovas <loumiakas1@gmail.com>
2020-12-31 19:21:43 +0000

    added rfcat setup step

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ apt update && apt upgrade
 apt install python3-pip -y
 pip3 install ansible tmuxp click==7.1.2
 ansible-playbook /vagrant/setup.yml --limit localhost --become
+ansible-playbook /vagrant/rfcat.yml --limit localhost --become
