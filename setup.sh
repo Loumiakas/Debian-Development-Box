b5f7be16fa87f045a8ccbee743e7923ac8c371f3
Ruslan Laskov <ruslan.laskov@aciworldwide.com>
2021-01-02 02:30:36 +0000

    added nRF sniffer setup and configuration steps 2

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ apt install python3-pip -y
 pip3 install ansible tmuxp click==7.1.2
 ansible-playbook /vagrant/setup.yml --limit localhost --become
 ansible-playbook /vagrant/rfcat.yml --limit localhost --become
+ansible-playbook /vagrant/nrfsniffer.yml --limit localhost --become
